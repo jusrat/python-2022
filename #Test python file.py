fd9554214be89da77722582f08b9aba49e8b6eb9
jusrat <100097085+jusrat@users.noreply.github.com>
2022-03-03 12:36:14 +1300

    my work

---
+++ #Test python file.py
@@ -0,0 +1,12 @@
+#1 my name
+print("Justyce Rata")
+#2 teachers name
+print("L Smith")
+#3 school name
+print("kings hgih school")
+#4 age
+print("15")
+#5 hours in favourite game
+print("40 hours in jojo")
+#6 what year would it be in 25 years
+print (25+22)
