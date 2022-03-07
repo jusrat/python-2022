ba7e66245121559215376826fab24785c16065b5
jusrat <100097085+jusrat@users.noreply.github.com>
2022-03-08 09:34:48 +1300

    yoza

---
+++ exercise_6.py
@@ -0,0 +1,35 @@
+#1
+print("press any key and then press enter")
+#2
+print("trype your name and then press enter")
+#3
+print("type your name and push enter")
+#4
+name = input("what is your name")
+#5
+age = input("what is your age")
+#6
+fav_movie = input("what is your fav movie")
+#7
+book = input("what is the name of the book")
+#8
+adjective = input("give me an adjective")
+#9
+noun = input("give me a noun")
+#10
+verb = input("give me a verb")
+#11
+print(f"{name} i like that, its got a ring to it")
+#12
+print(f"oh your {age} thats nice")
+#13
+print(f"I like {fav_movie} too")
+#14
+print(f"oh you read {book} too")
+#15
+print(f"i dont really like {adjective} that much")
+#16
+print(f"yeah i do {noun} too its pretty fun")
+#17
+print(f"oua {verb} fun")
+#18
